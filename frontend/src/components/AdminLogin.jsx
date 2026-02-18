5340635047e5637210e633be58983577eed0daf8
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2026-02-18 11:31:27 +0530

    Update AdminLogin.jsx

--- frontend/src/components/AdminLogin.jsx
+++ frontend/src/components/AdminLogin.jsx
@@ -16,7 +16,7 @@ function AdminLogin() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://smrtalumnicon.onrender.com/api/auth/login",
         { username, password }
       );
 
