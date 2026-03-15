9197e532f174d4310ca90b91160b16586b0a32c0
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2026-03-15 19:01:57 +0530

    Refactor Home component layout and styling

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,26 +7,44 @@ function Home() {
 
   return (
     <div
-      className="h-screen w-full bg-cover bg-center relative font-sans"
+      className="h-screen w-full bg-cover bg-center font-sans p-5 flex
+             justify-center items-center
+             sm:items-start sm:justify-center"
       style={{ backgroundImage: `url(${collegeImg})` }}
     >
-      {/* ===== NAVBAR ===== */}
-      <div className="absolute top-8 left-12 right-12 flex justify-between items-center md:flex-row flex-col md:gap-0 gap-3 md:top-8 md:left-12 md:right-12 top-4 left-4 right-4 bg-white/20 p-4 rounded-2xl">
-        <div className="text-[#00ff66] font-BigShInTxt text-4xl ">
+      <div className="bg-white/90 sm:bg-white/25 flex flex-col justify-around items-center text-center
+                  sm:flex-row sm:justify-between sm:items-center
+                  gap-4 sm:gap-0
+                  w-full max-w-5xl
+                  p-5 rounded-2xl">
+
+        {/* CSBS */}
+        <div className="text-[#0d9945] sm:text-[#00ff66] 
+                    font-BigShInTxt 
+                    text-3xl text-6xl sm:text-4xl">
           CSBS
         </div>
 
+        {/* Title */}
+        <div className="text-[#0d9945] 
+                    font-BigShInTxt
+                    text-2xl sm:text-3xl">
+          SMART ALUMNI CONNECT
+        </div>
+
+        {/* Login Button */}
         <button
           onClick={() => navigate("/login")}
           className="bg-[#00ff66] hover:bg-[#00e65c] transition
-                     px-6 py-2 rounded-full font-bold text-black text-sm md:text-base"
+               px-6 py-2 rounded-full font-bold text-black
+               text-sm sm:text-base w-full sm:w-auto"
         >
           LOGIN
         </button>
-      </div>
+
 
       {/* ===== RIGHT SIDE LOGIN TEXT ===== */}
-      <div
+      {/* <div
         className="absolute top-32 right-10 text-center text-[#00ff66] font-bold
                    md:top-32 md:right-10
                    static md:absolute mt-32 md:mt-0"
@@ -35,7 +53,7 @@ function Home() {
         <p className="mt-2 leading-tight text-base md:text-lg">
           PLEASE <br /> LOGIN
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
