68ce4139f6276773a73519378b6844a088fb0f21
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2026-04-01 19:25:10 +0530

    Update StudentHome.jsx

--- frontend/src/components/StudentHome.jsx
+++ frontend/src/components/StudentHome.jsx
@@ -219,8 +219,8 @@ function StudentHome() {
             </h2>
 
             {/* Banner Display */}
-            <div className="flex justify-center">
-              <div className="w-[250px] overflow-hidden">
+            <div className="flex justify-center items-center">
+              <div className="w-full max-w-4xl overflow-hidden">
                 {banners.length > 0 ? (
                   <img
                     src={banners[currentIndex].imageUrl}
